37f037781d645483f79afb8c934991de4fb904d1
vedhaduj <97368023+vedhaduj@users.noreply.github.com>
2022-05-19 19:46:03 -0400

    Update bmi.cpp

--- bmi.cpp
+++ bmi.cpp
@@ -2,20 +2,27 @@
 
 int main()
 {
-    std::cout << "Enter weight in kg: \n";
-    float weight;
-    std::cin >> weight;
+    int heiin;
+    int ibweight;
+    std::cout << "Enter height in inches: \n";
+    std::cin >> heiin;
+   
+    std::cout << "Enter weight in lbs: \n";
+    std::cin >> ibweight;
+   
+    int bmi = ibweight * 703;
 
-    std::cout << "Enter height as a floating decimal: \n";
-    std::cout << "Example | 5.11, 6.10, 5.09 etc...\n";
-    float height;
-    std::cin >> height;
+    int final_bmi = bmi / (heiin * heiin); //I could have done order of operations but I felt like it would be more 
+                                           //understandable like this.
+    
 
-    int f = weight / height;
+    std::cout << "BMI: " << final_bmi << std::endl;
+    
+    std::cout << "----------------------------\n";
 
-    std::cout << "Your bmi is: " << f << std::endl;
-    std::cout << "\n";
-    std::cout << "Below 18: Underweight || 18.5 - 24.9: Normal Weight || 25.0 - 29.9: Overweight\n";
-    std::cout << "30.0 - 34.9: Obese || 35.0 - 39.9 : Obesity Class 2 || above 40 : Obesity class 3\n";
-    std::cout << "Keep in mind this is psuedo accurate so round it up to the nearest tenth! - or fifth\n";
+    std::cout << "Underweight: less than 18.5\n";
+    std::cout << "Normal: between 18.5 and 24.9\n";
+    std::cout << "Overweight: between 25 and 29.9\n";
+    std::cout << "Obese: 30 or greater\n";
+    
 }
